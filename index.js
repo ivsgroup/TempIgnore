e91768fc46b342898919e3a1f533b86414cb7ff8
Florent Kolosowski <fkolosowski@ivsweb.com>
2015-11-05 10:46:21 +0100

    add module

---
+++ index.js
@@ -0,0 +1,56 @@
+var Class = require('uclass');
+
+var TempIgnore = new Class({
+
+  Implements : [
+    require('uclass/options')
+  ],
+
+  ignore_list : [],
+  timeout     : 5000,
+  timer       : null,
+
+  stack : function(customer_guid) {
+    this.ignore_list.push({
+      'customer_guid' : customer_guid,
+      'datetime' : new Date().getTime()
+    });
+  },
+
+  is_in : function(customer_guid) {
+    this.purge_old();
+
+    var matching_entries = Array.filter(this.ignore_list, function(value, key) {
+      return customer_guid == value.customer_guid;
+    });
+
+    var is_in = matching_entries.length > 0;
+    if (is_in)
+      this._destack(matching_entries[0]);
+
+    return is_in
+  },
+
+  _destack : function(entry) {
+    this.ignore_list.erase(entry);
+  },
+
+  purge_old : function() {
+    var self = this;
+
+    if (this.timer)
+      clearInterval(this.timer);
+
+    if (this.ignore_list.length > 0) {
+      this.timer = setInterval(function() {
+        var old_entries = Array.filter(self.ignore_list, function(value, key) {
+          return value.datetime + self.timeout < new Date().getTime();
+        });
+        if (old_entries.length > 0)
+          self._destack(old_entries[0]);
+        if (self.ignore_list.length == 0)
+          clearInterval(self.timer);
+      }, 1000);
+    }
+  }
+};
